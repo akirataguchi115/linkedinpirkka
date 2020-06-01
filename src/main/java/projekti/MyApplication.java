385a04d8556be5f11cbbce0925de2ba4bd950213
Akira Taguchi <akirahattivatti@gmail.com>
2020-06-01 15:29:38 +0300

    Trying to fix Heroku

--- src/main/java/projekti/MyApplication.java
+++ src/main/java/projekti/MyApplication.java
@@ -8,6 +8,43 @@ public class MyApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MyApplication.class);
+         /*
+        1.
+            1. Internetin perusosat
+            2. Web-palvelimen toiminta
+            3. Spring-sovelluskehitys
+        2.
+            1. Näkymät ja data
+            2. Tiedon lähettäminen palvelimelle
+            3. Tietokantojen käsittely ohjelmallisesti
+            4. Polkumuuttujat
+        3.
+            1. Useampi tietokantataulu ja taulujen väliset viitteet
+            2. Tietokantatransaktiot
+            3. N+1 kyselyn ongelma
+            4. Tietokantakyselyiden tulosten rajaaminen ja järjestäminen
+        4.
+            1. Mediatyyppi ja tiedostojen käsittely
+            2. Muutama sana ohjelmistokehityksestä
+            3. Sovelluksen rakenne
+            4. Konfiguraatioprofiilit
+            5. Sovellusten testaaminen
+            6. Sovelluksen siirtäminen pilvipalveluun
+        5.
+            1. HTTP-protokollan tilattomuus ja evästeet
+            2. Autentikaatio ja autorisaatio
+            3. Näkymä-ja metoditason autorisointi
+        6.
+            1. Toistuvat rakenteet HTML-sivuilla
+            2. Tyylitiedostot ja sivun asettelun määrittely
+            3. Syötteen validointi
+            4. Rajapinnat ja REST
+        7.
+            1. Selainohjelmisto ja Javascript
+            2. Muutama sana tietoturvasta
+            3. Isommille käyttäjämäärille skaalautuvat sovellukset
+            4. Reaktiivinen ohjelmointi
+        */
     }
 
 }
