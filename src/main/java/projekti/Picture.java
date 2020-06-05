26f85a5b084bc45c6b04df7f805a87fa34bda2c6
Akira Taguchi <akirahattivatti@gmail.com>
2020-06-05 11:19:22 +0300

    modified database structure to support connections with accept-feature

--- src/main/java/projekti/Picture.java
+++ src/main/java/projekti/Picture.java
@@ -2,6 +2,7 @@ package projekti;
 
 import java.util.List;
 import javax.persistence.Basic;
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.Lob;
---
+++ src/main/java/projekti/PictureRepository.java
@@ -0,0 +1,6 @@
+package projekti;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PictureRepository extends JpaRepository<Picture, Long> {
+}
