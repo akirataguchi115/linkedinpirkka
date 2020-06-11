95c54346e06423876a0efed03665759892f605f7
Akira Taguchi <akirahattivatti@gmail.com>
2020-06-11 17:54:49 +0300

    trying to fix heroku lob problem yet again

--- src/main/java/projekti/Picture.java
+++ src/main/java/projekti/Picture.java
@@ -1,15 +1,12 @@
 package projekti;
 
 import java.util.List;
-import javax.persistence.Basic;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.Lob;
 import javax.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.context.annotation.Profile;
+import org.hibernate.annotations.Type;
 import org.springframework.data.jpa.domain.AbstractPersistable;
 
 @Entity
@@ -22,9 +19,11 @@ public class Picture extends AbstractPersistable<Long> {
     private String name;
     private String mediaType;
     private Long size;
-
-    @Lob
-    @Basic(fetch = FetchType.LAZY)
+    //use this annotation when in Heroku environment(PostgreSQL)
+    @Type(type = "org.hibernate.type.BinaryType")
+//    @Lob
+//    @Basic(fetch = FetchType.LAZY)
+//use these when in local H2-environment
     private byte[] content;
     @OneToMany(mappedBy = "picture")
     private List<Account> accounts;
